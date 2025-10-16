d2deca326b8ebdc7955e4217f4eaf612ced13906
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-16 10:40:03 +0530

    Message

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app=express()
 app.get("/",(req,res)=>{
    res.status(200).json({message:"Hello This Side"})
 })
+app.get("/signin",(req,res)=>{
+    res.status(200).json({message:"User Login Succesfull"})
+})
 
 const port=3000
 app.listen(port,()=>{
