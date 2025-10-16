6077ebd780af664d4939580d9d8d694586488ebf
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-16 13:47:15 +0530

    Useringage feature Adding

---
+++ Pages/UserIngage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserIngage() {
+  return (
+    <div>UserIngage</div>
+  )
+}
+
+export default UserIngage
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app=express()
 app.get("/",(req,res)=>{
    res.status(200).json({message:"Hello This Side Saurabh Singh Backend Developer"})
 })
+app.get("/useringage",(req,res)=>{
+    res.status(200).json({message:"UserIngage Show Succesful When This feature Avail On Server"})
+})
 
 const port=3000
 app.listen(port,()=>{
